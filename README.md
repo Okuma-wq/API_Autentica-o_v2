b92ad6d230d534079e99d6a926a98df8c785131f
Pedro Laurentino Okuma <65199541+Okuma-wq@users.noreply.github.com>
2025-08-29 01:01:55 +0000

    Primeiro commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# API_Autentica-o_v2
---
+++ middleware/upload.js
@@ -0,0 +1,4 @@
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+module.exports = multer({ storage, limits: { fileSize: 10 * 1024 * 1024 } });
